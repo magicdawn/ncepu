1f92f5c979c05c9535d7289a6e4e45f9804458c8
magicdawn <784876393@qq.com>
2014-06-10 20:28:50 +0800

    2014-6-10 20:28:49

--- index.js
+++ index.js
@@ -1,58 +1,37 @@
 // var program = require("commander");
 var open = require("open");
 var colors = require("colors");
-
-//链接
-var urls = {
-    home: "http://ncepubd.tk/",
-
-    vod: "http://v.ncepubd.tk/",
-    down: "http://d.ncepubd.tk/",
-    bbs: "http://www.ncepubbs.cn/",
-    mybits: "http://m.ncepubd.tk/",
-
-    jiaowuchu: "http://j.ncepubd.tk/",
-    library: "http://202.206.214.254:8080/opac/",
-    chachengji: "http://59.67.225.73/te/"
-};
-
-var shorts = {
-    v: "vod",
-    d: 'down',
-    b: "bbs",
-    m: "mybits",
-
-    j: "jiaowuchu",
-    l: "library",
-    c: "chachengji",
-
-    jiao: "j",
-    lib: "l",
-    cha: "c"
-};
+var config = require("./config.json");
+var version = require("./package.json").version;
 
 var args = process.argv;
 var url;
 
 if (args.length > 2) {
     url = args[2];
-    while (shorts[url]) {
-        url = shorts[url];
+    while (config.shorts[url]) {
+        url = config.shorts[url];
     }
-    var url_to_open = urls[url];
-    console.log("正在打开 : %s", url_to_open.green);
-    open(url_to_open);
+
+    if (config.urls[url]) {
+        var url_to_open = config.urls[url];
+        console.log("正在打开 : %s", url_to_open.green);
+        open(url_to_open);
+    }
+    else{
+        console.log("未配置网址,可自行在config.json中配置...".red);
+    }
 }
 else {
     //ncepu
     console.log("使用说明 : ncepu <url>");
     console.log("   url可选值为:")
     console.log();
-    console.log("   home : 保定校区主页");
+    console.log("   home                        : 保定校区主页");
     console.log("   vod/v down/d bbs/b mybits/m : VOD/DOWM铺/缘路有你/MyBits");
-    console.log("   jiaowuchu/jiao/j : 教务处");
-    console.log("   library/lib/l : 图书馆");
-    console.log("   chachengji/cha/c : 教务处查成绩那个东西");
+    console.log("   jiaowuchu/jiao/j            : 教务处");
+    console.log("   library/lib/l               : 图书馆");
+    console.log("   chachengji/cha/c            : 教务处查成绩那个东西");
     console.log();
-    console.log("   BY Magicdawn".cyan)
+    console.log("   BY Magicdawn".cyan + " v" + version.yellow);
 }
